0883e0b14b1ded6c3399d712cabfd52422e7f2d8
bintang ary <wardhanabintang07@gmail.com>
2023-09-29 21:58:13 +0700

    done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,27 +1,47 @@
+import { useEffect, useState } from "react";
 import axios from "axios";
-import { useEffect } from "react";
 
 const Home = () => {
-  // Untuk hit API database dari TMDB
+  const [popularMovies, setPopularMovies] = useState([]);
+
   useEffect(() => {
     const getPopularMovies = async () => {
       try {
-        const response = await axios.get("https://api.themoviedb.org/3/movie/popular?language=en-US&page=1", {
+        const response = await axios.get(`${import.meta.env.VITE_API_URL}/3/movie/popular?language=en-US&page=1`, {
           headers: {
-            Authorization:
-              "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiYjA5NjYyZDcwNDI5ODFhNThkNThiNjljYTBiZDc0OCIsInN1YiI6IjY1MTQyMzIwOWI4NjE2MDBmZjY3OTU1NyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.F7fbXCr5b6imvT0wId4a4eLiSzo5U_nWEUlTRMFKdfw",
+            Authorization: `Bearer ${import.meta.env.VITE_API_TOKEN}`,
           },
         });
         const { data } = response;
-        console.log(data);
+
+        setPopularMovies(data?.results);
       } catch (error) {
-        alert(error);
+        if (axios.isAxiosError(error)) {
+          alert(error?.response?.data?.status_message);
+          return;
+        }
+
+        alert(error?.message);
       }
     };
+
     getPopularMovies();
   }, []);
 
-  return;
+  if (popularMovies.length === 0) {
+    return <h1>Loading....</h1>;
+  }
+
+  return (
+    <>
+      {popularMovies.map((movie) => (
+        <div key={movie?.id}>
+          <h1>{movie?.title}</h1>
+          <p>{movie?.overview}</p>
+        </div>
+      ))}
+    </>
+  );
 };
 
 export default Home;
